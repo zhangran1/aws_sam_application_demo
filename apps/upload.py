7de9ff07df8ed152516d5a7504a210fe2dfef610
Zhang Ran <zhang.ran@stengg.com>
2020-09-25 21:17:49 +0800

    add logging and remove default documentation

--- apps/upload.py
+++ apps/upload.py
@@ -1,7 +1,12 @@
+import os
 import json
+import logging
 
-# import requests
 
+logger = logging.getLogger()
+logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-6s [%(filename)s:%(lineno)d] %(message)s',
+                    datefmt='%Y-%m-%d:%H:%M:%S',
+                    level=os.environ.get("LOGLEVEL", "INFO"))
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
@@ -11,32 +16,18 @@ def lambda_handler(event, context):
     event: dict, required
         API Gateway Lambda Proxy Input Format
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
     context: object, required
         Lambda Context runtime methods and attributes
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
     Returns
     ------
-    API Gateway Lambda Proxy Output Format: dict
 
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
+            "message": "File uploaded successfully",
         }),
     }
