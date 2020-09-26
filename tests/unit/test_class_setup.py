e85b95e7af8d572d63573986c222dc529cbca231
Zhang Ran <zhang.ran@stengg.com>
2020-09-26 21:52:04 +0800

    Setup one valid and one invalid UploadDataParser object to validate object validation

--- tests/unit/test_class_setup.py
+++ tests/unit/test_class_setup.py
@@ -1,5 +1,7 @@
 import sys
 import logging
+import csv
+import base64
 import pytest
 
 sys.path.append('././apps/')
@@ -11,8 +13,24 @@ logger = logging.getLogger()
 
 
 @pytest.fixture(scope="function")
-def upload_data_parser():
-    logger.info("\n---------------setup------------------")
-    upload_data_parser = parser.UploadDataParser(VALID_PROCESSED_DATA_TEST_CASE_1)
-    yield upload_data_parser
-    logger.info("\n---------------tear down------------------")
+def valid_upload_data_parser():
+    logger.info("\n---------------setup valid upload data------------------")
+
+    data = VALID_PROCESSED_DATA_TEST_CASE_1
+    record = data[0]
+    csv_data = csv.reader(record.splitlines(), delimiter=',')
+    valid_upload_data_parser = parser.UploadDataParser(csv_data)
+    yield valid_upload_data_parser
+    logger.info("\n---------------tear down valid upload data------------------")
+
+
+@pytest.fixture(scope="function")
+def invalid_upload_data_parser():
+    logger.info("\n---------------setup invalid upload data------------------")
+
+    data = INVALID_EMPTY_FILE_PROCESSED_DATA
+    record = data[0]
+    csv_data = csv.reader(record.splitlines(), delimiter=',')
+    invalid_upload_data_parser = parser.UploadDataParser(csv_data)
+    yield invalid_upload_data_parser
+    logger.info("\n---------------tear down invalid upload data------------------")
