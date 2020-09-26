748466a2fa2aff199dd162aea4272b39574a354a
Zhang Ran <zhang.ran@stengg.com>
2020-09-26 20:48:51 +0800

    Update test cases for empty file test case

--- tests/unit/test_cases.py
+++ tests/unit/test_cases.py
@@ -9,10 +9,10 @@ import pytest
 
 from tests.unit.test_constants import *
 
-valid_test_case_2 = dir_path + "/sample_test_case/valid_test_case2.csv"
-invalid_test_case_1 = dir_path + "/sample_test_case/invalid_test_case_1.csv"
-invalid_test_case_2 = dir_path + "/sample_test_case/invalid_test_case_2.csv"
-invalid_test_case_3 = dir_path + "/sample_test_case/invalid_test_case_3.csv"
+valid_test_case_2 = dir_path + "/sample_test_case/valid_test_case2_with_chinese_character.csv"
+invalid_test_case_1 = dir_path + "/sample_test_case/invalid_test_case1_extra_one_column_record.csv"
+invalid_test_case_2 = dir_path + "/sample_test_case/invalid_test_case2_missing_one_column_record.csv"
+invalid_test_case_3 = dir_path + "/sample_test_case/invalid_test_case3_invalid_salary.csv"
 invalid_test_empty_file = dir_path + "/sample_test_case/invalid_test_empty_file.csv"
 
 
@@ -24,8 +24,7 @@ def user_input_valid_test_case_1():
     Content-Type: application/x-www-form-urlencoded
 
     multi-form data:
-        test_key: test_key_value
-        file: content of valid_test_case1.csv
+        file: content of valid_test_case1_with_hash.csv
     """
     valid_test_case_1 = TEST_CASE_TEMPLATE
     valid_test_case_1["body"] = BINARY_VALID_TEST_CASE_1
@@ -38,6 +37,12 @@ def user_input_valid_test_case_2():
         "body": valid_test_case_2
     }
 
+
+@pytest.fixture()
+def user_input_valid_test_case_3():
+    return {
+        "body": None
+    }
 
 @pytest.fixture()
 def user_input_invalid_test_case_1():
@@ -62,6 +67,14 @@ def user_input_invalid_test_case_3():
 
 @pytest.fixture()
 def user_input_invalid_test_empty_file():
-    return {
-        "body": invalid_test_empty_file
-    }
+    """
+    This is a invalid test case with empty csv file uploaded
+    Content-Encoding: utf-8
+    Content-Type: application/x-www-form-urlencoded
+
+    multi-form data:
+        file: content of invalid_test_empty_file.csv
+    """
+    empty_file_test_case = TEST_CASE_TEMPLATE
+    empty_file_test_case["body"] = BINARY_INVALID_EMPTY_FILE
+    return empty_file_test_case
