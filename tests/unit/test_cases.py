b3b64ffdaeb8e2e71346549afb928c0b341583a5
Zhang Ran <zhang.ran@stengg.com>
2020-09-26 21:17:29 +0800

    Update test cases for valid test case 3 and invalid test case 1, invalid test case 2

--- tests/unit/test_cases.py
+++ tests/unit/test_cases.py
@@ -1,17 +1,14 @@
 import sys
 import os
+import pytest
+
+from tests.unit.test_constants import *
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 sys.path.append('././apps/')
 
-import pytest
 
-from tests.unit.test_constants import *
-
-
-invalid_test_case_1 = dir_path + "/sample_test_case/invalid_test_case1_extra_one_column_record.csv"
-invalid_test_case_2 = dir_path + "/sample_test_case/invalid_test_case2_missing_one_column_record.csv"
 invalid_test_case_3 = dir_path + "/sample_test_case/invalid_test_case3_invalid_salary.csv"
 invalid_test_empty_file = dir_path + "/sample_test_case/invalid_test_empty_file.csv"
 
@@ -48,22 +45,47 @@ def user_input_valid_test_case_2():
 
 @pytest.fixture()
 def user_input_valid_test_case_3():
-    return {
-        "body": None
-    }
+    """
+    This is a valid test case
+    Content-Encoding: utf-8
+    Content-Type: application/x-www-form-urlencoded
+
+    multi-form data:
+        file: content of valid_test_case3_with_last_line_empty.csv
+    """
+    valid_test_case_3 = TEST_CASE_TEMPLATE
+    valid_test_case_3["body"] = BINARY_VALID_TEST_CASE_3
+    return valid_test_case_3
+
 
 @pytest.fixture()
 def user_input_invalid_test_case_1():
-    return {
-        "body": invalid_test_case_1
-    }
+    """
+    This is a valid test case
+    Content-Encoding: utf-8
+    Content-Type: application/x-www-form-urlencoded
+
+    multi-form data:
+        file: content of invalid_test_case1_extra_one_column_record.csv
+    """
+    invalid_test_case_1 = TEST_CASE_TEMPLATE
+    invalid_test_case_1["body"] = BINARY_INVALID_TEST_CASE_1
+    return invalid_test_case_1
 
 
 @pytest.fixture()
 def user_input_invalid_test_case_2():
-    return {
-        "body": invalid_test_case_2
-    }
+    """
+    This is a valid test case
+    Content-Encoding: utf-8
+    Content-Type: application/x-www-form-urlencoded
+
+    multi-form data:
+        file: content of invalid_test_case2_missing_one_column_record.csv
+    """
+    invalid_test_case_2 = TEST_CASE_TEMPLATE
+    invalid_test_case_2["body"] = BINARY_INVALID_TEST_CASE_2
+    return invalid_test_case_2
 
 
 @pytest.fixture()
