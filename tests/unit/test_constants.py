db1e6df78de707aa9e824ce0b13fc6391d616f81
Zhang Ran <zhang.ran@stengg.com>
2020-09-26 21:08:23 +0800

    Add binary data and processed data for invalid test case 1

--- tests/unit/test_constants.py
+++ tests/unit/test_constants.py
@@ -39,6 +39,7 @@ BINARY_VALID_TEST_CASE_3 = "LS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLTk1MzQ4NDczNzc0
                            "cG90dGVyMTAsaHBvdHRlcjEwIFBvdHRlciwxMzQyMzQuNQoNCi0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tL" \
                            "S05NTM0ODQ3Mzc3NDY2MjE5MTg2ODcwMjQtLQ0K"
 
+BINARY_INVALID_TEST_CASE_1 = "LS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLTQ4MzQyMTg0NjQxNzE1NzQ5ODEwMTgzNw0KQ29udGVudC1EaXNwb3NpdGlvbjogZm9ybS1kYXRhOyBuYW1lPSJmaWxlIjsgZmlsZW5hbWU9ImludmFsaWRfdGVzdF9jYXNlMV9leHRyYV9vbmVfY29sdW1uX3JlY29yZC5jc3YiDQpDb250ZW50LVR5cGU6IHRleHQvY3N2DQoNCkhlYWRpbmcgSW5mb3JtYXRpb24sLCwNCmUwMDAxLGhwb3R0ZXIxLGhwb3R0ZXIxIFBvdHRlciwxMjM0LjAwDQplMDAwMixocG90dGVyMixocG90dGVyMiBXZWFzbGV5LDE5MjM0LjUwDQplMDAwMyxocG90dGVyMyxocG90dGVyMyBTbmFwZSw0MDAwLjANCmUwMDA0LGhwb3R0ZXI0LGhwb3R0ZXI0IFBvdHRlciw0MDAwLjAsIDYwMDANCmUwMDA1LGhwb3R0ZXI1LGhwb3R0ZXI1IFBvdHRlciw1MjMuNA0KZTAwMDYsaHBvdHRlcjYsaHBvdHRlcjYgV2Vhc2xleSw0MDAwLjAwNA0KZTAwMDcsaHBvdHRlcjcsaHBvdHRlcjcgU25hcGUsMC4wDQplMDAwOCxocG90dGVyOCxocG90dGVyOCBQb3R0ZXIsMzQuMjMNCmUwMDA5LGhwb3R0ZXI5LGhwb3R0ZXI5IFBvdHRlciwzNDIzNC41DQplMDAwMTAsaHBvdHRlcjEwLGhwb3R0ZXIxMCBQb3R0ZXIsMTM0MjM0LjUNCmUwMDAxMSxocG90dGVyMTEsaHBvdHRlcjExIFBvdHRlciw0MA0KZTAwMDEyLGhwb3R0ZXIxMixocG90dGVyMTIgUG90dGVyLDUwDQotLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tNDgzNDIxODQ2NDE3MTU3NDk4MTAxODM3LS0NCg=="
 
 BINARY_INVALID_EMPTY_FILE = "LS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLTA1NDg5NTU4NTgzMzg2OTkxODY3NjA5Mg0KQ29udGVudC1E" \
                             "aXNwb3NpdGlvbjogZm9ybS1kYXRhOyBuYW1lPSJmaWxlIjsgZmlsZW5hbWU9ImludmFsaWRfdGVzdF9lbXB0e" \
@@ -92,6 +93,23 @@ VALID_PROCESSED_DATA_TEST_CASE_3 = ['Content-Disposition: form-data; name="file"
                         '\n#e00010,hpotter10,1hpotter'
                         '\ne0009,hpotter9,hpotter9 Potter,34234.5'
                         '\ne00010,hpotter10,hpotter10 Potter,134234.5\n\r\n', '--\r\n']
+
+INVALID_PROCESSED_DATA_TEST_CASE_1 = ['Content-Disposition: form-data; name="file"; '
+                                      'filename="invalid_test_case1_extra_one_column_record.csv"\r'
+                                      '\nContent-Type: text/csv\r\n\r'
+                                      '\nHeading Information,,,\r'
+                                      '\ne0001,hpotter1,hpotter1 Potter,1234.00\r'
+                                      '\ne0002,hpotter2,hpotter2 Weasley,19234.50\r'
+                                      '\ne0003,hpotter3,hpotter3 Snape,4000.0\r'
+                                      '\ne0004,hpotter4,hpotter4 Potter,4000.0, 6000\r'
+                                      '\ne0005,hpotter5,hpotter5 Potter,523.4\r'
+                                      '\ne0006,hpotter6,hpotter6 Weasley,4000.004\r'
+                                      '\ne0007,hpotter7,hpotter7 Snape,0.0\r'
+                                      '\ne0008,hpotter8,hpotter8 Potter,34.23\r'
+                                      '\ne0009,hpotter9,hpotter9 Potter,34234.5\r'
+                                      '\ne00010,hpotter10,hpotter10 Potter,134234.5\r'
+                                      '\ne00011,hpotter11,hpotter11 Potter,40\r'
+                                      '\ne00012,hpotter12,hpotter12 Potter,50\r\n', '--\r\n']
 
 
 INVALID_EMPTY_FILE_PROCESSED_DATA = ['Content-Disposition: form-data; name="file"; '
