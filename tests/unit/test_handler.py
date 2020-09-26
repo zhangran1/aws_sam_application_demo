3511332260f1942a05504a3c161a9d484932f209
Zhang Ran <zhang.ran@stengg.com>
2020-09-26 09:52:15 +0800

    Update test cases to handle two valid input and four invalid input

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -3,11 +3,51 @@ import json
 
 from apps import upload
 from tests.unit.test_cases import *
+from tests.unit.test_constants import *
+
 
 sys.path.append('././apps/')
+
 
 
 def test_user_input_valid_test_case_1(user_input_valid_test_case_1, mocker):
     ret = upload.lambda_handler(user_input_valid_test_case_1, "")
     data = json.loads(ret["body"])
-    assert ret["statusCode"] == 200
+    assert data["message"] == SUCCESS_MSG_RESPONSE
+    assert ret["statusCode"] == HTTP_SUCCESS_STATUS
+
+
+def test_user_input_valid_test_case_2(user_input_valid_test_case_2, mocker):
+    ret = upload.lambda_handler(user_input_valid_test_case_2, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == SUCCESS_MSG_RESPONSE
+    assert ret["statusCode"] == HTTP_SUCCESS_STATUS
+
+
+def test_user_input_invalid_test_case_1(user_input_invalid_test_case_1, mocker):
+    ret = upload.lambda_handler(user_input_invalid_test_case_1, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == FAIL_VALIDATION_FAIL
+    assert ret["statusCode"] == HTTP_FAIL_STATUS
+
+
+def test_user_input_invalid_test_case_2(user_input_invalid_test_case_2, mocker):
+    ret = upload.lambda_handler(user_input_invalid_test_case_2, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == FAIL_VALIDATION_FAIL
+    assert ret["statusCode"] == HTTP_FAIL_STATUS
+
+
+def test_user_input_invalid_test_case_3(user_input_invalid_test_case_3, mocker):
+    ret = upload.lambda_handler(user_input_invalid_test_case_3, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == FAIL_VALIDATION_FAIL
+    assert ret["statusCode"] == HTTP_FAIL_STATUS
+
+
+def test_invalid_test_empty_file(invalid_test_empty_file, mocker):
+    ret = upload.lambda_handler(invalid_test_empty_file, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == FAIL_VALIDATION_FAIL
+    assert ret["statusCode"] == HTTP_FAIL_STATUS
+
