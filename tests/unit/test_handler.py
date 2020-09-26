b52b4e745f49cf4046b0279ef6ba631daac580a5
Zhang Ran <zhang.ran@stengg.com>
2020-09-26 21:54:44 +0800

    Add additional test cases for user story 1

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,11 +1,11 @@
 import sys
-import json
 
 sys.path.append('././apps/')
 
 from apps import upload
 from tests.unit.test_cases import *
 from tests.unit.test_constants import *
+from tests.unit.test_class_setup import *
 from apps.http_responses import *
 from apps.utils import *
 
@@ -28,45 +28,82 @@ def test_http_response_fail():
 
 def test_filter_csv_data():
     processed_data = filter_csv_data(BINARY_VALID_TEST_CASE_1)
-    assert processed_data == VALID_TEST_CASE_PROCESSED_DATA
+    logger.info(processed_data)
+    assert processed_data == VALID_PROCESSED_DATA_TEST_CASE_1
 
 
-def test_user_input_valid_test_case_1(user_input_valid_test_case_1, mocker):
+def test_filter_csv_data_test_case_3():
+    processed_data = filter_csv_data(BINARY_VALID_TEST_CASE_3)
+    logger.info(processed_data)
+    assert processed_data == VALID_PROCESSED_DATA_TEST_CASE_3
+
+
+def test_valid_multi_part_form_parser(valid_upload_data_parser):
+    assert valid_upload_data_parser.valid_input is True
+
+
+def test_invalid_upload_data_parser(invalid_upload_data_parser):
+    assert invalid_upload_data_parser.valid_input is False
+
+
+def test_user_input_valid_test_case_1(user_input_valid_test_case_1):
     ret = upload.lambda_handler(user_input_valid_test_case_1, "")
     data = json.loads(ret["body"])
     assert data["message"] == SUCCESS_MSG_RESPONSE
     assert ret["statusCode"] == HTTP_SUCCESS_STATUS
 
-# def test_user_input_valid_test_case_2(user_input_valid_test_case_2, mocker):
-#     ret = upload.lambda_handler(user_input_valid_test_case_2, "")
-#     data = json.loads(ret["body"])
-#     assert data["message"] == SUCCESS_MSG_RESPONSE
-#     assert ret["statusCode"] == HTTP_SUCCESS_STATUS
-#
-#
-# def test_user_input_invalid_test_case_1(user_input_invalid_test_case_1, mocker):
-#     ret = upload.lambda_handler(user_input_invalid_test_case_1, "")
-#     data = json.loads(ret["body"])
-#     assert data["message"] == FAIL_VALIDATION_FAIL
-#     assert ret["statusCode"] == HTTP_FAIL_STATUS
-#
-#
-# def test_user_input_invalid_test_case_2(user_input_invalid_test_case_2, mocker):
-#     ret = upload.lambda_handler(user_input_invalid_test_case_2, "")
-#     data = json.loads(ret["body"])
-#     assert data["message"] == FAIL_VALIDATION_FAIL
-#     assert ret["statusCode"] == HTTP_FAIL_STATUS
-#
-#
-# def test_user_input_invalid_test_case_3(user_input_invalid_test_case_3, mocker):
-#     ret = upload.lambda_handler(user_input_invalid_test_case_3, "")
-#     data = json.loads(ret["body"])
-#     assert data["message"] == FAIL_VALIDATION_FAIL
-#     assert ret["statusCode"] == HTTP_FAIL_STATUS
-#
-#
-# def test_invalid_test_empty_file(invalid_test_empty_file, mocker):
-#     ret = upload.lambda_handler(invalid_test_empty_file, "")
-#     data = json.loads(ret["body"])
-#     assert data["message"] == FAIL_VALIDATION_FAIL
-#     assert ret["statusCode"] == HTTP_FAIL_STATUS
+
+def test_user_input_valid_test_case_2(user_input_valid_test_case_2):
+    ret = upload.lambda_handler(user_input_valid_test_case_2, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == SUCCESS_MSG_RESPONSE
+    assert ret["statusCode"] == HTTP_SUCCESS_STATUS
+
+
+def test_user_input_valid_test_case_3(user_input_valid_test_case_3):
+    ret = upload.lambda_handler(user_input_valid_test_case_3, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == SUCCESS_MSG_RESPONSE
+    assert ret["statusCode"] == HTTP_SUCCESS_STATUS
+
+
+def test_user_input_invalid_test_case_1(user_input_invalid_test_case_1):
+    ret = upload.lambda_handler(user_input_invalid_test_case_1, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == FAIL_VALIDATION_FAIL
+    assert ret["statusCode"] == HTTP_FAIL_STATUS
+
+
+def test_user_input_invalid_test_case_2(user_input_invalid_test_case_2):
+    ret = upload.lambda_handler(user_input_invalid_test_case_2, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == FAIL_VALIDATION_FAIL
+    assert ret["statusCode"] == HTTP_FAIL_STATUS
+
+
+def test_user_input_invalid_test_case_3(user_input_invalid_test_case_3):
+    ret = upload.lambda_handler(user_input_invalid_test_case_3, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == FAIL_VALIDATION_FAIL
+    assert ret["statusCode"] == HTTP_FAIL_STATUS
+
+
+def test_user_input_invalid_test_case_4(user_input_invalid_test_case_4):
+    ret = upload.lambda_handler(user_input_invalid_test_case_4, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == FAIL_VALIDATION_FAIL
+    assert ret["statusCode"] == HTTP_FAIL_STATUS
+
+
+def test_user_input_invalid_duplicate_id_login(user_input_invalid_duplicate_id_login):
+    ret = upload.lambda_handler(user_input_invalid_duplicate_id_login, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == FAIL_VALIDATION_FAIL
+    assert ret["statusCode"] == HTTP_FAIL_STATUS
+
+
+def test_user_input_invalid_test_empty_file(user_input_invalid_test_empty_file):
+    ret = upload.lambda_handler(user_input_invalid_test_empty_file, "")
+    data = json.loads(ret["body"])
+    assert data["message"] == FAIL_VALIDATION_FAIL
+    assert ret["statusCode"] == HTTP_FAIL_STATUS
