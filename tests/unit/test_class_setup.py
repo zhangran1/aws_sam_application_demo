cf63dd1ed848fce80119c79d291c5969e70a0eb5
Zhang Ran <zhang.ran@stengg.com>
2020-09-27 10:36:58 +0800

    Update test case for test employee class

--- tests/unit/test_class_setup.py
+++ tests/unit/test_class_setup.py
@@ -7,6 +7,7 @@ import pytest
 sys.path.append('././apps/')
 
 from apps import multipart_form_parser as parser
+from apps import employee
 from tests.unit.test_constants import *
 
 logger = logging.getLogger()
@@ -32,3 +33,11 @@ def invalid_upload_data_parser():
     invalid_upload_data_parser = parser.UploadDataParser(csv_data)
     yield invalid_upload_data_parser
     logger.info("\n---------------tear down invalid upload data------------------")
+
+
+@pytest.fixture(scope="function")
+def employee_object():
+    logger.info("\n---------------setup new employee------------------")
+    employee_object = employee.Employee("test00001", "john1", "John Smith", 100.5)
+    yield employee_object
+    logger.info("\n---------------tear down new employee------------------")
