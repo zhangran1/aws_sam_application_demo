ccee3701e93a2cdfc4775842257a8c0955961fe7
Zhang Ran <zhang.ran@stengg.com>
2020-09-27 14:59:41 +0800

    Add get and release database lock when upload file

--- apps/utils.py
+++ apps/utils.py
@@ -2,6 +2,7 @@ import base64
 import database_helper
 
 from constants import *
+from database_helper import *
 
 def filter_csv_data(multi_form_data):
     """Process event body input and return processed data.
@@ -42,10 +43,16 @@ def upload_csv_record_to_db(employee_records):
 
     """
     upload_status = DB_FAILED_OPERATION
+    database_helper.get_db_lock()
     for single_employee_record in employee_records:
         upload_status = database_helper.create_employee(single_employee_record)
         if upload_status == DB_FAILED_OPERATION:
             break
+    database_helper.release_db_lock()
 
     return upload_status
+
+
+def get_db_lock_status(db_lock=database_lock_id):
+    return check_db_lock(db_lock)
 
