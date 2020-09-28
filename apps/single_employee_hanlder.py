527b7d0b4173aad31d6cb019bff9a14750e819aa
Zhang Ran <zhang.ran@stengg.com>
2020-09-28 22:11:35 +0800

    Add single employee curd handler template function, actual logic to be implemented

---
+++ apps/single_employee_hanlder.py
@@ -0,0 +1,29 @@
+import logging
+import csv
+import http_responses
+import multipart_form_parser
+
+from utils import *
+from constants import *
+
+logger = logging.getLogger()
+
+
+def lambda_handler(event, context):
+    """Lambda Function to process CRUD request for single user. User ID shall be passed from path parameter. fields to
+    be processed will be passed in from body field.
+
+   Args:
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+    Returns:
+        Json response contains the following fields:
+        1. statusCode: 200 (Valid Response); 400 (Invalid Request);
+        2. body: Json data contains return standard message.
+    """
+
+    return http_responses.http_standard_return(True)
