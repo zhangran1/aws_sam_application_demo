3e7327d2b3dc3bac4768c148bcf78f1411519038
Zhang Ran <zhang.ran@stengg.com>
2020-09-29 16:09:15 +0800

    Add missing parameter handling

--- apps/get_user_by_id.py
+++ apps/get_user_by_id.py
@@ -24,5 +24,11 @@ def lambda_handler(event, context):
         1. statusCode: 200 (Valid Response); 400 (Invalid Request);
         2. body: Single employee records if statusCode is 200, fail response message will return if statusCode is 400.
     """
+    print(event["pathParameters"])
+    if not event["pathParameters"]:
+        return http_responses.http_standard_return(False, failed_msg=RETRIEVE_EMPLOYEE_FAILED)
+
+    if not event["pathParameters"]["id"]:
+        http_responses.http_standard_return(False, failed_msg=RETRIEVE_EMPLOYEE_FAILED)
 
     return database_helper.retrieve_user_record_by_id(event["pathParameters"]["id"])
