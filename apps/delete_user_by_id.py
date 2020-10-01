f171c62f7ad0d792105574cca3604b9d31ed45fc
Zhang Ran <zhang.ran@stengg.com>
2020-10-01 19:40:05 +0800

    Add lambda hanlder for api response of delete user

--- apps/delete_user_by_id.py
+++ apps/delete_user_by_id.py
@@ -18,5 +18,15 @@ def lambda_handler(event, context):
         1. statusCode: 200 (Valid Response); 400 (Invalid Request);
         2. body: Json data contains return standard message.
     """
+    if not event["pathParameters"]:
+        return http_responses.http_standard_return(False, failed_msg=DELETE_EMPLOYEE_FAILED)
 
-    return http_responses.http_standard_return(True)
+    if not event["pathParameters"]["id"]:
+        http_responses.http_standard_return(False, failed_msg=DELETE_EMPLOYEE_FAILED)
+
+    db_delete_operation = delete_employee(event["pathParameters"]["id"])
+
+    if db_delete_operation != DB_SUCCESS_OPERATION:
+        return http_responses.http_standard_return(False, failed_msg=DB_FAILED_OPERATION)
+
+    return http_responses.http_standard_return(True, success_msg=DB_SUCCESS_OPERATION)
